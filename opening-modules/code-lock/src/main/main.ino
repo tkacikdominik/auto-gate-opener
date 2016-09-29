e47e8737603641e2c32ea504b92e422d009c38f6
Dominik Tkáčik <tkacik38@gmail.com>
2016-09-29 18:44:56 +0200

    Code-Lock V0.1

---
+++ opening-modules/code-lock/src/main/main.ino
@@ -0,0 +1,33 @@
+#include <Key.h>
+#include <Keypad.h>
+
+/****************************** 
+***INICIALIZACIA KLAVESTNICE*** 
+*******************************/ 
+
+const byte rows = 4;
+const byte cols = 4;
+char keys[rows][cols] = 
+{
+{'1','2','3','A'},
+{'4','5','6','B'},
+{'7','8','9','C'},
+{'*','0','#','D'}
+};
+byte rowsPin[rows] = {50, 51, 52, 53};
+byte colsPin[cols] = {46, 47, 48, 49};
+Keypad keyPad = Keypad(makeKeymap(keys), rowsPin, colsPin, rows, cols);
+
+void setup() 
+{
+ Serial.begin(9600);
+}
+
+void loop() 
+{
+char charRead = keyPad.getKey();
+if (charRead)
+{
+Serial.println(charRead);
+}
+}
