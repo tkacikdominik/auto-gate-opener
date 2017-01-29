30e82f7a305274fea056ccd9407f2c0ccbceb132
tkacikdominik <tkacik38@gmail.com>
2017-01-29 22:14:53 +0100

    Summar dps

--- masters/arduinoMaster/master/src/master/master.ino
+++ masters/arduinoMaster/master/src/master/master.ino
@@ -1,7 +1,7 @@
-#include <SPI.h>
+  #include <SPI.h>
 #include <RFM69.h>
 
-  #define MASTERADDRESS 1
+#define MASTERADDRESS 1
   #define CODELOCKADDRESS 2
   #define NETWORKADDRESS 0
   #define FREQUENCY     RF69_868MHZ
@@ -11,11 +11,11 @@
 
 
 /******P I N S********/
-const byte relay1 = 11;
-const byte relay2 = 3;
+const byte led1 = 6;
+const byte led2 = 7;
+const byte led3 = 8;
 
 /*******C O D E******/ 
-const byte massageLength = 10;
 const byte maxCodeLength = 8;
 const char emptySymbol = 'x';
           
@@ -24,9 +24,6 @@ char code[maxCodeLength] = {'0','9','0','5','1','9','9','7'};
 /**V A R I A B L E**/
 boolean check = true;
 
-//Status
-byte processStatus;
-
 void setup() 
 {
 
@@ -34,72 +31,32 @@ void setup()
   Serial.print("ArduinoMaster");
   radio.initialize(FREQUENCY, MASTERADDRESS, NETWORKADDRESS);
   radio.encrypt(ENCRYPTKEY);
-  pinMode(relay1, OUTPUT);
-  pinMode(relay2, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
   resetCode();
-  statusReset();
 }
 
 void loop() 
 {
-  switch(processStatus)
+  if (radio.receiveDone())
   {
-    case 0:Serial.println("Prijmam Kod,Status");
-    Serial.println(processStatus);
-      if (radio.receiveDone())
-      {
-        Serial.print("message [");
-        for (int i = 0; i < radio.DATALEN; i++)
-        {
-          readCode[i] = (char)radio.DATA[i];
-          Serial.print((char)radio.DATA[i]);
-        }
-        Serial.print("], RSSI ");
-        Serial.println(radio.RSSI);
-        if (radio.ACKRequested())
-        {
-          radio.sendACK();
-          Serial.println("Prijata sprava"); 
-        }
-      }
-    break;
-    
-    case 1:Serial.println("Prijmam Kod");
-    Serial.println(processStatus);
-    if (verifyCode()) statusUp();
-    else statusReset;
-    break;
-  }
-}
-
-boolean verifyCode()
-{
-  for(int i=0;i<maxCodeLength;i++)
-  {
-    if(readCode[i] != code[i])
+    Serial.print("message [");
+    int premenna = 0;
+    for (int i = 0; i < radio.DATALEN; i++)
     {
-      statusReset();
-      return false; 
+      readCode[i] = (char)radio.DATA[i];
+      Serial.print((char)radio.DATA[i]);
+    }
+    Serial.print("], RSSI ");
+    Serial.println(radio.RSSI);
+    if (radio.ACKRequested())
+    {
+      radio.sendACK();
+      Serial.println("Prijata sprava");
+      if (verifyCode())grantAccess(); 
     }
   }
-  statusUp();
-  resetCode();
-  return true;
-}
-
-void statusUp()
-{
-  processStatus=processStatus++;
-}
-
-void statusDown()
-{
-  processStatus=processStatus--;
-}
-
-void statusReset()
-{
-  processStatus=0;
 }
 
 void resetCode()
@@ -109,3 +66,32 @@ void resetCode()
     readCode[i]=0;  
   }
 }
+
+boolean verifyCode()
+{
+  for(int i=0;i<maxCodeLength;i++)
+  {
+    if(readCode[i] != code[i])
+    {
+      return false; 
+    }
+  }
+resetCode();
+return true;
+}
+void grantAccess()
+{    
+    Serial.println("Grant Access");
+    digitalWrite(led1, HIGH);
+    delay(180);
+    digitalWrite(led2, HIGH);  
+    delay(180);
+    digitalWrite(led3, HIGH);
+    delay(180);
+    digitalWrite(led1, LOW);
+    delay(180);
+    digitalWrite(led2, LOW);  
+    delay(180);
+    digitalWrite(led3, LOW);                     
+    delay(180);
+} 
